66d808581191e0996dfd1c572c832589c78e8d53
lobabadru <127347894+lobabadru@users.noreply.github.com>
2024-12-06 13:04:17 +0100

    Update login.php

--- login.php
+++ login.php
@@ -107,7 +107,7 @@ include('include/header.php');
                      <ul class="location_icon">
                         <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i></a><br> Location</li>
                         <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i></a><br> +01 1234567890</li>
-                        <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a><br> demo@gmail.com</li>
+                        <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a><br> Tobiibadru@gmail.com</li>
                      </ul>
                   </div>
                </div>
@@ -116,7 +116,7 @@ include('include/header.php');
                <div class="container">
                   <div class="row">
                      <div class="col-md-12">
-                        <p>© 2019 All Rights Reserved. Design by<a href="https://html.design/"> Free Html Templates</a></p>
+                        <p>© 2019 All Rights Reserved. Design by<a href="https://html.design/">Badru Oluwatobi</a></p>
                      </div>
                   </div>
                </div>
